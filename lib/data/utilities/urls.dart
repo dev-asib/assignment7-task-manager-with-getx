4d7b9760edcb2dbc8d6313495bb6eb63d474936e
dev-asib <mail.asib.com@gmail.com>
2024-09-29 13:04:48 +0600

    Updated

--- lib/data/utilities/urls.dart
+++ lib/data/utilities/urls.dart
@@ -1,4 +1,4 @@
-class Urls{
+class Urls {
   static const String _baseUrl = 'http://152.42.163.176:2006/api/v1';
 
   static const String registration = '$_baseUrl/Registration';
@@ -9,11 +9,18 @@ class Urls{
   static const String inProgressTasks = '$_baseUrl/listTaskByStatus/Progress';
   static const String cancelledTasks = '$_baseUrl/listTaskByStatus/Cancelled';
   static const String taskStatusCount = '$_baseUrl/taskStatusCount';
+
   static String deleteTask(String id) => '$_baseUrl/deleteTask/$id';
-  static String updateTasksStatus(String taskID, String taskStatus) => '$_baseUrl/updateTaskStatus/$taskID/$taskStatus';
+
+  static String updateTasksStatus(String taskID, String taskStatus) =>
+      '$_baseUrl/updateTaskStatus/$taskID/$taskStatus';
   static const String profileUpdate = '$_baseUrl/ProfileUpdate';
-  static String recoverVerifyEmail(String email) => '$_baseUrl/RecoverVerifyEmail/$email';
-  static String otpVerification(String email, String otp) => '$_baseUrl/RecoverVerifyOtp/$email/$otp';
+
+  static String recoverVerifyEmail(String email) =>
+      '$_baseUrl/RecoverVerifyEmail/$email';
+
+  static String otpVerification(String email, String otp) =>
+      '$_baseUrl/RecoverVerifyOtp/$email/$otp';
   static const String recoverResetPass = '$_baseUrl/RecoverResetPassword';
 
 }
